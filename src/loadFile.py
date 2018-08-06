95c2a002810e1e5b971914c6b8a6d0b270f40643
FishLai <38682183+FishLai@users.noreply.github.com>
2018-08-06 22:30:24 +0800

    can't compile

--- src/loadFile.py
+++ src/loadFile.py
@@ -4,7 +4,10 @@ Created on 2018年8月6日
 @author: quan_
 '''
 import numpy as np
-
-def loadFile():
+#use numpy.genfromtxt could choose range
+def loadFile(directory, floor, ceiling):
+    x, y = np.genfromtxt(directory, skip_header = floor, skip_footer = ceiling, unpack = True)
+    return x, y
 if __name__ == '__main__':
-    pass
+    x, y = loadFile("C:/workspace/Data/test_ramanMapping/test-z7.9168y37.5310x44.7104.txt", 0, 0)
+    print(x)
