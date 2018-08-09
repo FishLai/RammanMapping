2a778c320f9bac314a640ca1567e4d1360ea1c2e
FishLai <pc@mybro>
2018-08-09 17:25:22 +0800

    load file method complete

---
+++ src/fileIO.py
@@ -0,0 +1,20 @@
+'''
+Created on 2018年8月7日
+
+@author: quan_
+'''
+import os
+
+
+
+def pichFile(directory):
+    f = []
+    isInfo = 0
+    for Path, dirs, fns in os.walk(directory):
+        for fn in fns:
+            if isInfo == 0 and "Info" in fn:
+                isInfo = 1
+                continue
+            filePth = os.path.join(Path, fn)
+            f.append(filePth)
+    return f
--- src/loadFile.py
+++
@@ -1,9 +0,0 @@
-'''
-Created on 2018年8月7日
-
-@author: quan_
-'''
-
-
-if __name__ == '__main__':
-    pass
